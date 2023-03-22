e1a5de5d8d24e100efc1af58fbb203bffbda8256
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2023-03-22 18:37:14 -0300

    Onboarding once in memory

--- Bank/Bank/AppDelegate.swift
+++ Bank/Bank/AppDelegate.swift
@@ -10,11 +10,13 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    var hasOnboarded = false
+    
     let loginViewController = LoginViewController()
     let onboardingContainerViewController = OnboardingContainerViewController()
     let dummyViewController = DummyViewController()
     
-    var hasOnboarded = false
+    
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -49,7 +51,11 @@ extension AppDelegate {
 
 extension AppDelegate: LoginViewControllerDelegate {
     func didLogin() {
-        setRootViewController(onboardingContainerViewController)
+        if hasOnboarded {
+            setRootViewController(dummyViewController)
+        } else {
+            setRootViewController(onboardingContainerViewController)    
+        }
     }
 }
 
