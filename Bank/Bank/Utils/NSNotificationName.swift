9904c98270afb551a148489edbb17b0535642d10
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2023-04-26 14:31:51 -0300

    refact: Add logout button and notification

---
+++ Bank/Bank/Utils/NSNotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotificationName.swift
+//  Bank
+//
+//  Created by Erasmo J.F Da Silva on 26/04/23.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let logout = Notification.Name("Logout")
+}
