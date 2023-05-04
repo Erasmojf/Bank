81b1918b81e74d6f7a4e334035158fa6d5861b4c
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2023-05-04 11:52:27 -0300

    feat: adding network

---
+++ Bank/Bank/Utils/Date+Utils.swift
@@ -0,0 +1,24 @@
+//
+//  Date+Utils.swift
+//  Bank
+//
+//  Created by Erasmo J.F Da Silva on 03/05/23.
+//
+
+import Foundation
+
+import Foundation
+
+extension Date {
+    static var bankeyDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "MDT")
+        return formatter
+    }
+    
+    var monthDayYearString: String {
+        let dateFormatter = Date.bankeyDateFormatter
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
