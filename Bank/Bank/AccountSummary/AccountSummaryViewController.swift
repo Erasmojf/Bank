45e04ca58b1eb66bb3fb701f8687d8570cb163fc
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2023-05-04 18:12:23 -0300

    feat: adding networking to account

--- Bank/Bank/AccountSummary/AccountSummaryViewController.swift
+++ Bank/Bank/AccountSummary/AccountSummaryViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class AccountSummaryViewController: UIViewController {
         //Request Models
     var profile: Profile?
+    var accounts: [Account] = []
     
         //View Models
     var headerViewModel = AccountSummaryHeaderView
@@ -98,37 +99,6 @@ extension AccountSummaryViewController: UITableViewDelegate {
     }
 }
 
-
-extension AccountSummaryViewController {
-    private func fetchAccounts() {
-        let savings = AccountSummaryCell.ViewModel(accountType: .Banking,
-                                                   accountName: "Basic Savings",
-                                                   balance: 929466.23)
-        let chequing = AccountSummaryCell.ViewModel(accountType: .Banking,
-                                                    accountName: "No-Fee All-In Chequing",
-                                                    balance: 17562.44)
-        let visa = AccountSummaryCell.ViewModel(accountType: .CreditCard,
-                                                accountName: "Visa Avion Card",
-                                                balance: 412.83)
-        let masterCard = AccountSummaryCell.ViewModel(accountType: .CreditCard,
-                                                      accountName: "Student Mastercard",
-                                                      balance: 50.83)
-        let investment1 = AccountSummaryCell.ViewModel(accountType: .Investment,
-                                                       accountName: "Tax-Free Saver",
-                                                       balance: 2000.00)
-        let investment2 = AccountSummaryCell.ViewModel(accountType: .Investment,
-                                                       accountName: "Growth Fund",
-                                                       balance: 15000.00)
-        
-        accountCellViewModels.append(savings)
-        accountCellViewModels.append(chequing)
-        accountCellViewModels.append(visa)
-        accountCellViewModels.append(masterCard)
-        accountCellViewModels.append(investment1)
-        accountCellViewModels.append(investment2)
-    }
-}
-
     //MARK: Actions
 extension AccountSummaryViewController {
     @objc func logoutTapped(sender: UIButton){
@@ -151,7 +121,16 @@ extension AccountSummaryViewController {
             }
         }
         
-        fetchAccounts()
+        fetchAccounts(forUserId: "1") { result in
+            switch result {
+            case .success(let accounts):
+                self.accounts = accounts
+                self.configureTableCells(with: accounts)
+                self.tableView.reloadData()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
     private func configureTableHeaderView(with profile: Profile) {
@@ -159,5 +138,11 @@ extension AccountSummaryViewController {
                                                     name: profile.firstName,
                                                     date: Date())
         headerView.configure(viewModel: vm)
+    }
+    
+    private func configureTableCells(with accounts: [Account]) {
+        accountCellViewModels = accounts.map {
+            AccountSummaryCell.ViewModel(accountType: $0.type, accountName: $0.name, balance: $0.amount)
+        }
     }
 }
