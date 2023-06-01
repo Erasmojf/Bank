14b89b52a3f1bdac55d87ba5af02d7c4e0cdacf7
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2023-06-01 14:50:43 -0300

    Add uniting view controllers

---
+++ Bank/BankUnitTests/AccountSummary/AccountSummaryViewControllerTests.swift
@@ -0,0 +1,25 @@
+//
+//  AccountSummaryViewControllerTests.swift
+//  BankUnitTests
+//
+//  Created by Erasmo J.F Da Silva on 01/06/23.
+//
+
+import Foundation
+import XCTest
+
+@testable import Bank
+
+class AccountSummaryViewControllerTests: XCTestCase {
+    var vc: AccountSummaryViewController!
+    
+    override func setUp() {
+        super.setUp()
+        vc = AccountSummaryViewController()
+        //vc.loadViewIfNeeded()
+    }
+    
+    func testSomething() throws {
+        
+    }
+}
